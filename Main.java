a8d55565cf7c3aa11f738f31b4dafc4da1003c19
yuruse419 <chaseorion19@gmail.com>
2023-10-31 16:59:37 -0500

    stage 2

--- Main.java
+++ Main.java
@@ -13,8 +13,9 @@ public class Main {
     boolean running = true;
 
     while(running) {
-      System.out.println("Enter the number for the corresponding action: ");
-      System.out.println("\t0. Quit \n\t1. Display Pets\n\t2. Insert Pet");
+      // prompt user for action
+      System.out.println("Enter the number for the corresponding action:");
+      System.out.println("\t0. Quit \n\t1. Display Pets\n\t2. Insert Pet\n\t3. Search Pets");
 
       // initialize a String for user input
       String userInput = inputObj.nextLine();
@@ -22,10 +23,11 @@ public class Main {
       // blank line
       System.out.println();
 
+      // quit
       if(userInput.equals("0")) {
         running = false;
       }
-      // display pets
+      // else display pets
       else if(userInput.equals("1")) {
         System.out.println(displayPets(pets));
       }
@@ -33,7 +35,7 @@ public class Main {
       else if(userInput.equals("2")) {
         // until user enters a string that contains "done"
         while(userInput.indexOf("done") == -1) {
-          System.out.println("\nEnter new pet info: <name> <age> done");
+          System.out.println("Enter new pet info: <name> <age> done");
 
           userInput = inputObj.nextLine().toLowerCase();
         }
@@ -49,6 +51,43 @@ public class Main {
           System.out.println("Invalid age");
         }
       }
+      // else search by pet name or age
+      else if(userInput.equals("3")) {
+        System.out.println("How do you wish to search?");
+
+        // until user chooses a valid action
+        while(!userInput.equals("1") && !userInput.equals("2")) {
+          System.out.println("\t1. By Name\n\t2. By Age");
+
+          userInput = inputObj.nextLine();
+        }
+
+        System.out.println("Enter search parameter:");
+
+        // search by name
+        if(userInput.equals("1")) {
+          String searchName = inputObj.nextLine().toLowerCase();
+
+          // blank line
+          System.out.println();
+
+          System.out.println(displayPets(pets, searchName));
+        }
+        // else try search by age
+        else if(userInput.equals("2")) {
+          try {
+            int searchAge = Integer.parseInt(inputObj.nextLine());
+
+            // blank line
+            System.out.println();
+
+            System.out.println(displayPets(pets, searchAge));
+          }
+          catch(NumberFormatException exception) {
+            System.out.println("Invalid age");
+          }
+        }
+      }
 
       // blank line
       System.out.println();
@@ -58,6 +97,7 @@ public class Main {
     inputObj.close();
   }
 
+  // generate table of pets
   public static String displayPets(ArrayList<Pet> pets) {
     String table = "ID\tName\tAge";
     table += "\n--\t----\t---";
@@ -68,6 +108,48 @@ public class Main {
 
     table += "\n--\t----\t---";
     table += "\nEntries: " + pets.size();
+
+    return table;
+  }
+
+  // override displayPets for name searching
+  public static String displayPets(ArrayList<Pet> pets, String filter) {
+    String table = "ID\tName\tAge";
+    table += "\n--\t----\t---";
+
+    int entries = 0;
+
+    for(int i = 0; i < pets.size(); i++) {
+      if(pets.get(i).getName().indexOf(filter) != -1) {
+        table += "\n" + i + "\t" + pets.get(i).getName() + "\t" + pets.get(i).getAge();
+
+        entries++;
+      }
+    }
+
+    table += "\n--\t----\t---";
+    table += "\nEntries: " + entries;
+
+    return table;
+  }
+
+  // override displayPets for age searching
+  public static String displayPets(ArrayList<Pet> pets, int filter) {
+    String table = "ID\tName\tAge";
+    table += "\n--\t----\t---";
+
+    int entries = 0;
+
+    for(int i = 0; i < pets.size(); i++) {
+      if(pets.get(i).getAge() == filter) {
+        table += "\n" + i + "\t" + pets.get(i).getName() + "\t" + pets.get(i).getAge();
+
+        entries++;
+      }
+    }
+
+    table += "\n--\t----\t---";
+    table += "\nEntries: " + entries;
 
     return table;
   }
