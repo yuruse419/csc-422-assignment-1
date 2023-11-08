6e4f4f3ef332efb68a75e5dcc3eccb7b155fa0a7
yuruse419 <chaseorion19@gmail.com>
2023-11-07 19:12:04 -0600

    stage 4, step 2 + other changes

--- Main.java
+++ Main.java
@@ -1,77 +1,77 @@
 import java.util.Scanner;
-import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws FileNotFoundException, IOException {
     // instantiate a Scanner object for user input
     Scanner inputObj = new Scanner(System.in);
 
-    // create a File object for the pets file
-    File petsFile = new File("stored_pets.txt");
-    
+    // create a Path reference to the pets file
+    Path petsFilePath = Paths.get("stored_pets.txt");
+
+    // if the pets file does not exist
+    if(!Files.exists(petsFilePath)) {
+      // create the pets file
+      Files.createFile(petsFilePath);
+    }
+
     // initialize empty Pets ArrayList
     ArrayList<Pet> pets = new ArrayList<Pet>();
 
-    try {
-      // create a Scanner object for the pets file
-      Scanner storedPets = new Scanner(petsFile);
+    // create Iterator for the pets file
+    Iterator<String> petsDataIterator = Files.readAllLines(petsFilePath).listIterator();
 
-      // while a next pet entry exists
-      while(storedPets.hasNextLine()) {
-        // if five (5) pet entries have already been added
-        if(pets.size() >= 5) {
-          // notify of capacity reached
-          System.out.println("Pet capacity reached.");
+    // while a next pet entry exists
+    while(petsDataIterator.hasNext()) {
+      // if five (5) pet entries have already been added
+      if(pets.size() >= 5) {
+        // notify of capacity reached
+        System.out.println("Pet capacity reached.");
 
-          // terminate pet insertions
-          break;
-        }
-
-        // store the current pet entry and advance the pointer to the next pet entry
-        String petEntry = storedPets.nextLine();
-
-        try {
-          // try to store the index of the name–age delimiter
-          int delimiterIndex = petEntry.indexOf(" ");
-  
-          // try to parse pet name and age
-          String petName = petEntry.substring(0, delimiterIndex);
-          int petAge = Integer.parseInt(petEntry.substring(delimiterIndex + 1));
-  
-          // if age between 0 and 21, exclusively
-          if(petAge > 0 && petAge < 21) {
-            // add pet to pets ArrayList
-            pets.add(new Pet(petName, petAge));
-          }
-          // else if age out of bounds
-          else {
-            System.out.println("Invalid entry skipped (age out of bounds)...");
-          }
-        }
-        // catch for invalid age
-        catch(NumberFormatException exception) {
-          System.out.println("Invalid entry skipped (non-numerical age)...");
-        }
-        // catch for absence of delimiter (i.e., " ")
-        catch(IndexOutOfBoundsException exception) {
-          System.out.println("Invalid entry skipped (no delimiter)...");
-        }
+        // terminate pet insertions
+        break;
       }
 
-      // close the pets file
-      storedPets.close();
-    }
-    // catch for no pets file
-    catch(FileNotFoundException exception) {
-      System.out.println("No file to load from. Continuing...");
+      // store the next pet entry and advance the pointer
+      String petEntry = petsDataIterator.next();
+
+      try {
+        // try to store the index of the name–age delimiter
+        int delimiterIndex = petEntry.indexOf(" ");
+
+        // try to parse pet name and age
+        String petName = petEntry.substring(0, delimiterIndex);
+        int petAge = Integer.parseInt(petEntry.substring(delimiterIndex + 1));
+  
+        // if age between 0 and 21, exclusively
+        if(petAge > 0 && petAge < 21) {
+          // add pet to pets ArrayList
+          pets.add(new Pet(petName, petAge));
+        }
+        // else if age out of bounds
+        else {
+          System.out.println("Invalid entry skipped (age out of bounds)...");
+        }
+      }
+      // catch for invalid age
+      catch(NumberFormatException exception) {
+        System.out.println("Invalid entry skipped (non-numerical age)...");
+      }
+      // catch for absence of delimiter (i.e., " ")
+      catch(IndexOutOfBoundsException exception) {
+        System.out.println("Invalid entry skipped (no delimiter)...");
+      }
     }
 
-    // boolean for keeping the system running
-    boolean running = true;
-
-    while(running) {
+    // user-controlled loop
+    while(true) {
       // prompt user for action
       System.out.println("\nEnter the number for the corresponding action:");
       System.out.println("\t0. Quit \n\t1. Display Pets\n\t2. Insert Pet\n\t3. Search Pets\n\t4. Edit Pet\n\t5. Remove Pet");
@@ -81,7 +81,7 @@ public class Main {
 
       // quit
       if(userInput.equals("0")) {
-        running = false;
+        break;
       }
       // else display pets
       else if(userInput.equals("1")) {
@@ -89,11 +89,14 @@ public class Main {
       }
       // else insert pet
       else if(userInput.equals("2")) {
-        // prompt for pet info or termination
-        System.out.println("\nEnter new pet info or type \"done\" to finish. \n\tFormat: <name> <age>");
+        // if stored pets is not at capacity
+        if(pets.size() < 5) {
+          // prompt for pet info or termination
+          System.out.println("\nEnter new pet info or type \"done\" to finish. \n\tFormat: <name> <age>");
+        }
 
         // loop for pet-info input
-        while(true) {
+        while(true && pets.size() < 5) {
           // store input
           userInput = inputObj.nextLine().toLowerCase();
 
@@ -103,12 +106,20 @@ public class Main {
           }
 
           try {
-            // parse input
-            String petName = userInput.substring(0, userInput.indexOf(" "));
-            int petAge = Integer.parseInt(userInput.substring(petName.length() + 1));
+            // try to store the index of the name–age delimiter
+            int delimiterIndex = userInput.indexOf(" ");
+
+            // try to parse input
+            String petName = userInput.substring(0, delimiterIndex);
+            int petAge = Integer.parseInt(userInput.substring(delimiterIndex + 1));
   
             // add to pets ArrayList
             pets.add(new Pet(petName, petAge));
+
+            byte[] newPetAsBytes = (petName + " " + petAge + "\n").getBytes();
+
+            // add pet to pets file
+            Files.write(petsFilePath, newPetAsBytes, StandardOpenOption.APPEND);
 
             // output confirmation
             System.out.println("Pet added.");
@@ -117,6 +128,12 @@ public class Main {
           catch(NumberFormatException exception) {
             System.out.println("Invalid age");
           }
+        }
+
+        // if stored pets is at capacity
+        if(pets.size() >= 5) {
+          // notify user to remove a pet to store another 
+          System.out.println("\nPets capacity reached. Please remove a pet to store another.");
         }
       }
       // else search by pet name or age
@@ -213,7 +230,7 @@ public class Main {
       }
     }
 
-    // close the Scanner object
+    // close the user-input Scanner object
     inputObj.close();
   }
 
